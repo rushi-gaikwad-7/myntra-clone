ceaba66472dffd4fb87c8c86906a8847c481002f
rushig777 <gaikwadrushi777@gmail.com>
2022-02-23 23:42:40 +0530

    updated !

--- js/studio.js
+++ js/studio.js
@@ -2,45 +2,69 @@
 
 studioData = [
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/a9f68785-e282-425a-b270-c978c387b0f31597840342635-Content-ethnicwear-color-whites.jpg"
+        pimg:"https://static.toiimg.com/photo/msid-80933483/80933483.jpg?157288",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/0d1e0a28-3088-4719-a692-4cdaa7a33cc71597840342726-Content-ethnicwear-occasion-casuallook.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9d248917-d1b0-4910-8de0-4ed7c2b4af8e1595935030939-Content-ethnicwear-trends-printedkurtaset.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/83d9ca97-4aa3-46ce-bd28-b135d3b94a021595935030673-Content-ethnicwear-essentials-everydaykurtas.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9e4fb95e-6268-49c5-9ed1-e6b1bd4b5efd1595935030880-Content-ethnicwear-trend-fashionmeetcomfort.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/f0397d0f-9c2a-4c87-956e-9896b615b3061597840342772-Content-ethnicwear-trend-printparadise.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/87b0957c-41f7-4522-9f30-f69087a0dce61597840342593-Content-ethnicwear-color-mustard.jpg"
-    },
-    {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/2ff5c4e9-da10-4d12-9aae-86c38f4c281a1597840342549-Content-ethnicwear-brand-fusiondress.jpg"
-    },
-    {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/8/21/99463248-ae80-47de-836f-3bafe2262bff1598029618274-Content-mostselling-Ethnicwear-Kurtasetsbiba.jpg"
-    },
-    {
-        img: "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/e4fe9b08-9d0a-4349-868f-6ac5aa7123ed1595935030800-Content-ethnicwear-trend-Bsummerreadyethnicdresses.jpg"
+        pimg:"https://assets.myntassets.com/f_auto,q_100,dpr_1.8,w_411,c_limit,fl_progressive/assets/images/2021/11/3/fdb0263d-0d15-4642-abfb-b855c69e395b1635913009059-7C763B42-C4C2-4410-8970-51711D78C915.jpeg",
+        userid:"Genelia123",
+        follow:"follow",
+        img: "https://www.bollywoodhungama.com/wp-content/uploads/2021/07/Genelia-Deshmukh-ups-the-fashion-ante-as-she-pairs-off-leather-pants-with-formal-shirt.jpeg"
     },
     ];
     
     
     
-    studioData.map(function (el, i,arr) {
-    
+    studioData.map(function (el, i,arr) 
+    {
     var box = document.createElement("div");
+    box.setAttribute("id","mainbox")
+    var navbar= document.createElement("div");
+    navbar.setAttribute("id","postnav")
+    var dpimg = document.createElement("img");
+    dpimg.setAttribute("id","dp")
+    dpimg.setAttribute("src", el.pimg);
+    var id= document.createElement("h4")
+    id.innerText=el.userid;
+    id.setAttribute("id","id")
+    var follow= document.createElement("p")
+    follow.innerText=el.follow
+    follow.setAttribute("id","follow")
     var img = document.createElement("img");
     img.setAttribute("src", el.img);
-    
-    box.append(img);
-    document.querySelector("#indianwear").append(box);
+    navbar.append(dpimg,id,follow)
+    box.append(navbar,img);
+    document.querySelector("#postDiv").append(box);
     });
