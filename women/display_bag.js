071c098a8deefaa3b2b55bee76055c18297fc805
Tejaswini Mahale <tejaswinimahale11@gmail.com>
2022-02-27 11:39:57 +0530

    new update on display bag page

--- women/display_bag.js
+++ women/display_bag.js
@@ -12,7 +12,7 @@ function addtobagList(addtobag) {
     var priceDiv=document.createElement("div")
     priceDiv.setAttribute("class","priceDiv")
     var div1=document.createElement("div")
-     div1.setAttribute("id","div1")
+    div1.setAttribute("id","div1")
     var imgDiv=document.createElement("div")
     imgDiv.setAttribute("class","imgdiv")
     var textdiv=document.createElement("div")
@@ -37,7 +37,8 @@ function addtobagList(addtobag) {
     var image5=document.createElement("img")
     image5.setAttribute("src",elem.img5)
     
-    var description=document.createElement("P")
+    var description=document.createElement("h1")
+    description.setAttribute("class","description")
     description.innerText=elem.description
 
     var detail=document.createElement("p");
@@ -45,10 +46,11 @@ function addtobagList(addtobag) {
      detail.setAttribute("id","clr")
 
     var Size=document.createElement("p")
-    Size.innerText=elem.Size
+    Size.setAttribute("class","size")
+    Size.innerText="Size & Fit"+elem.Size
     
     var MaterialCare=document.createElement("p")
-    MaterialCare.innerText=elem.MaterialCare
+    MaterialCare.innerText="Material & care"+elem.MaterialCare
 
     var speccification=document.createElement("p")
     speccification.innerText=elem.speccification.SleeveLength;
@@ -76,6 +78,7 @@ function addtobagList(addtobag) {
 
     var brand=document.createElement("h1")
     brand.innerText=elem.brand
+    brand.setAttribute("class","brand")
     brand.style.fontWeight="bold"
 
     var name=document.createElement("p");
@@ -91,27 +94,63 @@ function addtobagList(addtobag) {
     discount.innerText="(" + elem.discount + "% OFF)" 
     discount.style.color="#de938e"
 
+    //------------------------------
+    var line4=document.createElement("p")
+    line4.setAttribute("class","line4")
+    line4.innerText="inclusive of all taxes"
+
+    var line5=document.createElement("p")
+    line5.setAttribute("class","line5")
+    line5.innerText="Select Size : S M L XL XXL"
+
+   var line6=document.createElement("p")
+   line6.setAttribute("class","line6")
+   line6.innerText="DELIVERY OPTIONS"
+
+   var input1=document.createElement("input")
+   input1.setAttribute("class","input1")
+   input1.placeholder="ENTER A PIN CODE"
+
+   var line7=document.createElement("p")
+   line7.innerText="Please enter PIN code to check delivery time & Pay on Delivery Availability"
+
+   var line8=document.createElement("div")
+   line8.setAttribute("class","line8")
+   line8.innerText="100% Original Products Pay on delivery might be available Easy 30 days returns and exchanges Try & Buy might be available"
+   
+   var line9=document.createElement("h1")
+   line9.setAttribute("class","line9")
+   line9.innerText="Best Offers";
+
+   var line10=document.createElement("h1")
+   line10.setAttribute("class","line10")
+   line10.innerText="PRODUCT DETAILS "
+
+
+   //---------------------------------------------
+
     var addBagButton=document.createElement("button")
     addBagButton.setAttribute("id","addbtn")
     addBagButton.innerText="ADD TO BAG";
     addBagButton.addEventListener("click", function () {
-      //bag(elem);
+      bag(elem);
       addToCart(elem);
-     // window.location.href ="bag.html";
     });
+
     var wishlistButton=document.createElement("button")
     wishlistButton.setAttribute("id","wishbtn")
     wishlistButton.innerText="WISHLIST"
     wishlistButton.addEventListener("click", function () {
     wishList(elem);
   });
-
-  var infodiv=document.createElement("div")
-  infodiv.append(bestoffers,detail,Size,MaterialCare,speccification,speccification1,speccification2,speccification3,speccification4,speccification5,speccification6,speccification7,speccification8)
   
+  
+  var infodiv=document.createElement("div")
+
+  infodiv.append(line10,detail,speccification,speccification1,speccification2,speccification3,speccification4,speccification5,speccification6,speccification7,speccification8)
    imgDiv.append(image,image1,image2,image3,image4,image5,) 
    div1.append(price,strikeprice,discount)
-   buttonDiv.append(addBagButton,wishlistButton)
+   buttonDiv.append(line4,line5,addBagButton,wishlistButton,line6,input1,line7,line8,line9,bestoffers)
    textdiv.append(brand,description,div1,buttonDiv,infodiv)
    maindiv.append(imgDiv,textdiv)
    document.querySelector("#info").append(maindiv)
@@ -138,23 +177,22 @@ function addtobagList(addtobag) {
 
 
   //-----------------------------add-to-bag-items----------------------//
-  // function bag(elem) {
+  function bag(elem) {
     
-  //   var object2 = {
-  //     img:elem.img,
-  //     name: elem.name,
-  //     brand: elem.brand,
-  //     price:elem.price,
-  //     strikeprice:elem.strikeprice,
-  //     discount:elem.discount
-  //   };
-  //   bagItems.push(object2);
-  //   localStorage.setItem("bagData", JSON.stringify(bagItems));
-  // }
+    var object2 = {
+      img:elem.img,
+      name: elem.name,
+      brand: elem.brand,
+      price:elem.price,
+      strikeprice:elem.strikeprice,
+      discount:elem.discount
+    };
+    bagItems.push(object2);
+    localStorage.setItem("bagData", JSON.stringify(bagItems));
+  }
 
   function addToCart(elem){
-    elem.quntity=1;
-    bagItems.push(elem);
-    console.log(bagItems)
+    el.quntity=1;
+    cardData.push(elem);
     localStorage.setItem("bagData",JSON.stringify(bagItems))
 }
